38f75f45c35a26fb4c64fc7adc948b4504578fd6
Michael Taylor <mtaylor@michaeltaylor.dev>
2025-09-02 20:47:46 -0400

    Update GitHub Actions workflows for icon embedding
    - Updated build-installers.yml to verify icon embedding after build
    - Added cleanup of temporary digstore.rc file in Windows builds
    - Updated release.yml to verify icon embedding for Windows builds
    - Added digstore.rc to .gitignore (generated by build script)
    - Icon embedding now uses embed-resource crate for reliability

--- build.rs
+++ build.rs
@@ -1,32 +1,53 @@
 // Build script to embed icon and set Windows executable metadata
 
 fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=DIG.ico");
-    
     // Only run on Windows
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        println!("cargo:warning=Running Windows build script");
+        println!("cargo:rerun-if-changed=build.rs");
+        println!("cargo:rerun-if-changed=DIG.ico");
+        println!("cargo:rerun-if-changed=digstore.rc");
         
-        // Create Windows resource file
-        let mut res = winres::WindowsResource::new();
+        // Create resource file with icon and version info
+        let rc_content = r#"
+#pragma code_page(65001)
+1 ICON "DIG.ico"
+
+1 VERSIONINFO
+FILEVERSION     0,1,0,0
+PRODUCTVERSION  0,1,0,0
+FILEFLAGSMASK   0x3f
+FILEFLAGS       0x0
+FILEOS          0x40004
+FILETYPE        0x1
+FILESUBTYPE     0x0
+BEGIN
+    BLOCK "StringFileInfo"
+    BEGIN
+        BLOCK "040904B0"
+        BEGIN
+            VALUE "CompanyName",      "DIG Network"
+            VALUE "FileDescription",  "Content-addressable storage system"
+            VALUE "FileVersion",      "0.1.0.0"
+            VALUE "InternalName",     "digstore"
+            VALUE "LegalCopyright",   "Copyright © 2025 DIG Network"
+            VALUE "OriginalFilename", "digstore.exe"
+            VALUE "ProductName",      "Digstore"
+            VALUE "ProductVersion",   "0.1.0.0"
+        END
+    END
+    BLOCK "VarFileInfo"
+    BEGIN
+        VALUE "Translation", 0x409, 0x4B0
+    END
+END
+"#;
         
-        // Set the icon - this embeds it in the .exe
-        res.set_icon("DIG.ico");
-        println!("cargo:warning=Icon set to DIG.ico");
+        // Write RC file
+        std::fs::write("digstore.rc", rc_content).expect("Failed to write RC file");
+        println!("cargo:warning=Created digstore.rc file");
         
-        // Set version info
-        res.set("ProductName", "Digstore");
-        res.set("CompanyName", "DIG Network");
-        res.set("LegalCopyright", "Copyright © 2025 DIG Network");
-        res.set("FileDescription", "Content-addressable storage system");
-        
-        // Compile the resource file
-        match res.compile() {
-            Ok(_) => println!("cargo:warning=Resource compilation successful"),
-            Err(e) => println!("cargo:warning=Resource compilation failed: {}", e),
-        }
-    } else {
-        println!("cargo:warning=Not Windows, skipping icon embedding");
+        // Use embed-resource to compile and link the resources
+        embed_resource::compile("digstore.rc", embed_resource::NONE);
+        println!("cargo:warning=Resources compiled and will be linked into executable");
     }
 }
